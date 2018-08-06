6b991e32fd89cd01066967c1e02a65c955c3ab8f
caemci <carlosecimino@gmail.com>
2018-08-06 14:21:24 -0300

    Miembros estáticos

--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -8,12 +8,13 @@ public class Principal {
         Persona jp = new Persona("Juan","Pérez",'M',"Argentina","12345678","09/07/1990","Soltero","Bulnes 1526 4to C","1123456789","jp90@gma1l.com");
         
         CuentaBancaria cb = new CuentaBancaria("jupe90", "Caja de ahorros", '$', jp);
+        CuentaBancaria cb2 = new CuentaBancaria("jupe90_2", "Caja de ahorros", 'U', jp);
+        CuentaBancaria cb3 = new CuentaBancaria("jupe-90", "Cuenta Corriente", '$', jp);
         
-        // Hay dos maneras de acceder a Juan Pérez:        
-        jp.setNombre("Pedro");
-        cb.getTitular().setApellido("Ramírez");
+        // Observá la correlatividad de los CBUs
         
-        jp.presentarse();
         System.out.println( cb );
+        System.out.println( cb2 );
+        System.out.println( cb3 );
     }
 }
