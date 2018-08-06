0f52639fb52baabfc69a0332bff617dce6ee0f82
caemci <carlosecimino@gmail.com>
2018-08-06 10:28:55 -0300

    Encapsulamiento

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -11,62 +11,44 @@ import java.time.format.DateTimeFormatter;
  */
 public class Persona {
     
-    String nombre;
-    String apellido;
-    char genero;
-    String paisDeOrigen;
-    String DNI;
-    String fechaDeNacimiento;
-    String estadoCivil;
-    String domicilio;
-    String movil;
-    String mail;
-    
-    /* 
-        Si no se escribe un constructor, Java implícitamente crea uno por defecto:
-    
-        Persona () {
-
-        }
-    
-        Si el programador escribe uno o más constructores, el constructor por defecto se anula.
+    /*
+        ENCAPSULAMIENTO: Se debe garantizar la integridad de los datos de un objeto. No se deben conocer los detalles de implementación.
+        Los atributos no se modifican directamente, sino que se utilizan métodos para lectura (getter) y escritura (setter).
+        Todos los atributos de un objeto deberían ser privados (únicamente accesibles desde dentro de la clase), con la posible excepción
+        de aquellos que sean constantes (final).
     */
+    private String nombre;
+    private String apellido;
+    private char genero;
+    /*
+        paisDeOrigen, DNI y fechaDeNacimineto no tienen método setter 
+        ni tampoco una manera de modificar su valor dentro de la misma clase.
+        Conceptualmente, se trata de constantes, por lo que llevan la palabra final delante (no es obligatorio).
+    */
+    private final String paisDeOrigen;
+    private final String DNI;
+    private final String fechaDeNacimiento;
+    private String estadoCivil;
+    private String domicilio;
+    private String movil;
+    private String mail;
     
-    // Constructor con todos los parámetros
-    public Persona(String nom, String ape, char gen, String pais, String DNI, String nac, String est, String dom, String mov, String mail) {
-        nombre = nom;
-        apellido = ape;
-        genero = gen;
-        paisDeOrigen = pais;
-        /*
-            Si se escribe: 
-                DNI = DNI
-            Se tiene una ambigüedad: ¿Cuál es el atributo y cuál es parámetro?
-            El uso de this rompe la ambiguedad. Refiere al objeto actual.
-            A través de this, el objeto accede a los atributos y métodos de sí mismo 
-        */
-        this.DNI = DNI; 
-        fechaDeNacimiento = nac;
-        estadoCivil = est;
-        domicilio = dom;
-        movil = mov;
-        this.mail = mail;
-    }
-    
-    // Constructor con algunos parámetros
-    public Persona(String nombre, String apellido) {
-        /*
-            Puede usarse el this en cualquier caso, no solo para resolver ambigüedades
-        */
+    /*
+        Un objeto se comunica con el "exterior" a través de sus métodos públicos.
+        Todo lo que sea public es accesible desde cualquier lugar.
+    */
+
+    public Persona(String nombre, String apellido, char genero, String paisDeOrigen, String DNI, String fechaDeNacimiento, String estadoCivil, String domicilio, String movil, String mail) {
         this.nombre = nombre;
         this.apellido = apellido;
-        this.paisDeOrigen = "n/a";
-        this.DNI = "n/a";
-        this.fechaDeNacimiento = "01/01/1900";
-        this.estadoCivil = "n/a";
-        this.domicilio = "n/a";
-        this.movil = "n/a";
-        this.mail = "n/a";
+        this.genero = genero;
+        this.paisDeOrigen = paisDeOrigen;
+        this.DNI = DNI;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.estadoCivil = estadoCivil;
+        this.domicilio = domicilio;
+        this.movil = movil;
+        this.mail = mail;
     }
 
     @Override // Indica "sobreescritura" o "redifinición". No es obligatorio, pero es deseable indicarlo.
@@ -74,29 +56,105 @@ public class Persona {
         return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", genero=" + genero + ", paisDeOrigen=" + paisDeOrigen + ", DNI=" + DNI + ", fechaDeNacimiento=" + fechaDeNacimiento + ", estadoCivil=" + estadoCivil + ", domicilio=" + domicilio + ", movil=" + movil + ", mail=" + mail + '}';
     }  
   
-    void presentarse () {
+    public void presentarse () {
         System.out.println( "\"Hola ! Soy " + nombreCompleto() + ", de " + paisDeOrigen +
                             ". Tengo " + obtenerEdad() + " años y me encuentro " + estadoCivil.toLowerCase() + 
                             ". Escribime a " + mail);
     }
+    
+    /* 
+        nombreCompleto() es un ejemplo de método privado. La devolución del nombre completo es una funcionalidad que
+        requiero dentro de la clase, pero no deseo que fuera de la misma se pueda obtener el nombre completo de la persona.
+        Por eso nombreCompleto() es private.
+        Que un método sea public o private depende del alcance a criterio del programador.
+    */
 
-    String nombreCompleto() {
+    private String nombreCompleto() {
         return nombre + " " + apellido;
     }
-
-    void cambiarDeEstadoCivil(String nuevoEstadoCivil) {
-        estadoCivil = nuevoEstadoCivil;
-    }
     
-    void cambiarDeDomicilio(String nuevoDomicilio) {
-        domicilio = nuevoDomicilio;
-    }
-    
-    int obtenerEdad () {
+    public int obtenerEdad () {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // Formato de fecha
         LocalDate fechaNac = LocalDate.parse(fechaDeNacimiento, fmt); // Fecha de nacimiento
         LocalDate ahora = LocalDate.now(); // Fecha actual
         Period periodo = Period.between(fechaNac, ahora); // Período entre las dos fechas
         return periodo.getYears(); // Cantidad de años del período
+    }
+    
+    /* 
+        Métodos getter (para obtener un atributo) y setter (para establecer un atributo).
+        Gracias a ellos, es posible garantizar la integridad del estado de un objeto.
+        Observá en detalle el método setMail()
+    */
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public String getPaisDeOrigen() {
+        return paisDeOrigen;
+    }
+    
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getMovil() {
+        return movil;
+    }
+
+    public void setMovil(String movil) {
+        this.movil = movil;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        // Solo se actualiza el mail si el String contiene un arroba.
+        if ( mail.contains("@") ) {
+            this.mail = mail;
+        }
     }
 }
--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -5,44 +5,20 @@ public class Principal {
 
     public static void main(String[] args) {
         
-        /* 
-            Persona jp = new Persona(); // ERROR
-            
-            No existe un constructor en la clase Persona que permita instanciar objetos sin parámetros.
-            Para que eso funcione, en la clase Persona debería haber un tercer constructor:
-        
-                Futbolista () {
-                    this.nombre = "n/a";
-                            ...
-                    this.mail = "n/a";
-                }   
-        */
-        
-        Persona jp = new Persona("Juan","Pérez",'M',"Argentina","12345678","09/07/1990","Soltero","Bulnes 1526 4to C","jp90@gma1l.com","1123456789");
-        Persona pr = new Persona("Pedro","Rodríguez");
+        Persona jp = new Persona("Juan","Pérez",'M',"Argentina","12345678","09/07/1990","Soltero","Bulnes 1526 4to C","1123456789","jp90@gma1l.com");
         
         jp.presentarse();
-        pr.presentarse();
         
-        /* 
-            Cuando se intenta mostrar un objeto con println(), Java invoca implícitamente al método toString() del mismo.
+        jp.setNombre("Luis");
         
-            Por lo tanto, escribir:
-                System.out.println( jp );
-            Es lo mismo que escribir:
-                System.out.println( jp.toString() );
+        jp.presentarse();
         
-            El método toString() existe en TODOS los objetos, ya que es heredado de la superclase Object.
-            Cuando el programador crea una clase, debería redefinir el método toString() con una salida a su gusto,
-            ya que de lo contrario, se invoca al toString() original de Object que muestra solo una representación
-            abstracta de la referencia de la instancia en memoria.
-            (Ejemplo: clases.Persona@27bc2616)
+        jp.setMail("mailIncorrecto");
         
-            En la clase Persona, se redefinió el método toString() para que la salida sea más descriptiva.
-        */
-        System.out.println("\nAl tener el toString() redefinido, se puede mostrar un objeto directamente por consola: ");
-        System.out.println( jp ); // Equivalente a: System.out.println( jp.toString() );
-        System.out.println( pr ); // Equivalente a: System.out.println( pr.toString() );
+        jp.presentarse();
+        
+        jp.setMail("lupe@gma1l.com");
+        
+        jp.presentarse();
     }
-    
 }
