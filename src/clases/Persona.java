ffb4d6a78ff305081ee701dbb7d873fb111ac283
caemci <carlosecimino@gmail.com>
2018-08-06 10:10:29 -0300

    Constructor y toString

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -21,6 +21,58 @@ public class Persona {
     String domicilio;
     String movil;
     String mail;
+    
+    /* 
+        Si no se escribe un constructor, Java implícitamente crea uno por defecto:
+    
+        Persona () {
+
+        }
+    
+        Si el programador escribe uno o más constructores, el constructor por defecto se anula.
+    */
+    
+    // Constructor con todos los parámetros
+    public Persona(String nom, String ape, char gen, String pais, String DNI, String nac, String est, String dom, String mov, String mail) {
+        nombre = nom;
+        apellido = ape;
+        genero = gen;
+        paisDeOrigen = pais;
+        /*
+            Si se escribe: 
+                DNI = DNI
+            Se tiene una ambigüedad: ¿Cuál es el atributo y cuál es parámetro?
+            El uso de this rompe la ambiguedad. Refiere al objeto actual.
+            A través de this, el objeto accede a los atributos y métodos de sí mismo 
+        */
+        this.DNI = DNI; 
+        fechaDeNacimiento = nac;
+        estadoCivil = est;
+        domicilio = dom;
+        movil = mov;
+        this.mail = mail;
+    }
+    
+    // Constructor con algunos parámetros
+    public Persona(String nombre, String apellido) {
+        /*
+            Puede usarse el this en cualquier caso, no solo para resolver ambigüedades
+        */
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.paisDeOrigen = "n/a";
+        this.DNI = "n/a";
+        this.fechaDeNacimiento = "01/01/1900";
+        this.estadoCivil = "n/a";
+        this.domicilio = "n/a";
+        this.movil = "n/a";
+        this.mail = "n/a";
+    }
+
+    @Override // Indica "sobreescritura" o "redifinición". No es obligatorio, pero es deseable indicarlo.
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", genero=" + genero + ", paisDeOrigen=" + paisDeOrigen + ", DNI=" + DNI + ", fechaDeNacimiento=" + fechaDeNacimiento + ", estadoCivil=" + estadoCivil + ", domicilio=" + domicilio + ", movil=" + movil + ", mail=" + mail + '}';
+    }  
   
     void presentarse () {
         System.out.println( "\"Hola ! Soy " + nombreCompleto() + ", de " + paisDeOrigen +
--- src/clases/Principal.java
+++ src/clases/Principal.java
@@ -4,22 +4,45 @@ package clases;
 public class Principal {
 
     public static void main(String[] args) {
-        Persona p1 = new Persona();
         
-        p1.nombre = "Juan";
-        p1.apellido = "Pérez";
-        p1.genero = 'M';
-        p1.paisDeOrigen = "Argentina";
-        p1.DNI = "12345678";
-        p1.fechaDeNacimiento = "09/07/1990";
-        p1.estadoCivil = "Soltero";
-        p1.domicilio = "Bulnes 1526 4to C";
-        p1.mail = "jp90@gma1l.com";
-        p1.movil = "1123456789";
+        /* 
+            Persona jp = new Persona(); // ERROR
+            
+            No existe un constructor en la clase Persona que permita instanciar objetos sin parámetros.
+            Para que eso funcione, en la clase Persona debería haber un tercer constructor:
         
-        p1.presentarse();
-        p1.cambiarDeEstadoCivil("En pareja");
-        p1.presentarse();
+                Futbolista () {
+                    this.nombre = "n/a";
+                            ...
+                    this.mail = "n/a";
+                }   
+        */
+        
+        Persona jp = new Persona("Juan","Pérez",'M',"Argentina","12345678","09/07/1990","Soltero","Bulnes 1526 4to C","jp90@gma1l.com","1123456789");
+        Persona pr = new Persona("Pedro","Rodríguez");
+        
+        jp.presentarse();
+        pr.presentarse();
+        
+        /* 
+            Cuando se intenta mostrar un objeto con println(), Java invoca implícitamente al método toString() del mismo.
+        
+            Por lo tanto, escribir:
+                System.out.println( jp );
+            Es lo mismo que escribir:
+                System.out.println( jp.toString() );
+        
+            El método toString() existe en TODOS los objetos, ya que es heredado de la superclase Object.
+            Cuando el programador crea una clase, debería redefinir el método toString() con una salida a su gusto,
+            ya que de lo contrario, se invoca al toString() original de Object que muestra solo una representación
+            abstracta de la referencia de la instancia en memoria.
+            (Ejemplo: clases.Persona@27bc2616)
+        
+            En la clase Persona, se redefinió el método toString() para que la salida sea más descriptiva.
+        */
+        System.out.println("\nAl tener el toString() redefinido, se puede mostrar un objeto directamente por consola: ");
+        System.out.println( jp ); // Equivalente a: System.out.println( jp.toString() );
+        System.out.println( pr ); // Equivalente a: System.out.println( pr.toString() );
     }
     
 }
